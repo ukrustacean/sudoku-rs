3db73936db6567ef1ab11e79ba6c37f2620cca9e
Ivan "ukrustacean" Dereviankin <ukrustacean@gmail.com>
2024-01-15 11:47:44 +0200

    Make it possible to pass sudoku from stdin

--- src/bin.rs
+++ src/bin.rs
@@ -1,5 +1,6 @@
 use sudoku_rs::Solver;
 
+#[cfg(not(feature = "read_from_stdin"))]
 const SUDOKU: &str = r#"
                 3, 1, 0, 0, 0, 0, 0, 2, 0,
                 0, 0, 6, 1, 0, 9, 0, 0, 5,
@@ -12,9 +13,24 @@ const SUDOKU: &str = r#"
                 0, 0, 0, 0, 0, 7, 0, 0, 0,
 "#;
 
+#[cfg(not(feature = "read_from_stdin"))]
 fn main() {
     let mut sudoku = SUDOKU.parse::<Solver>().expect("Sudoku must be valid");
     println!("{}", sudoku);
     sudoku.solve();
     println!("{}", sudoku);
+}
+
+#[cfg(feature = "read_from_stdin")]
+fn main() {
+    use std::io::Read;
+    let mut buf = String::new();
+    std::io::stdin()
+        .read_to_string(&mut buf)
+        .expect("Must be able to read sudoku from stdin");
+
+    let mut sudoku = buf.parse::<Solver>().expect("Sudoku must be valid");
+    sudoku.solve();
+    println!();
+    println!("{sudoku}");
 }
